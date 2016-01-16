7011b9f26a8d9f06c1578b23f81215630c3dbb28
Brian Chang <bchang85@hotmail.com>
2016-01-15 18:56:46 -0800

    Add in frames per second information

--- pong.js
+++ pong.js
@@ -105,7 +105,8 @@
         var now = window.performance.now();
         var timePassed = now - lastUpdate;
         lastUpdate = now;
-        var multiplier = timePassed / 1000;
+        var multiplier = timePassed / 1000.;
+        Game.framesPerSecond = Math.round(1000 / timePassed);
 
         ball.x += ball.velocity.x * multiplier;
         ball.y += ball.velocity.y * multiplier;
@@ -175,6 +176,10 @@
         context.font = "24px courier";
         context.textAlign = "center";
         context.fillText(scoreString, canvas.width / 2, 30);
+
+        context.font = "10px courier";
+        context.textAlign = "end";
+        context.fillText("FPS: " + Game.framesPerSecond, canvas.width, canvas.height);
     };
 
     Game.run = function () {
